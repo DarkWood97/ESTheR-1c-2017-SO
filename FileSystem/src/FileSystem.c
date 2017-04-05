08ae34c2400d52e2fa507aa5d64c09f3f7301600
samantha <samanthalexia@est.frba.utn.edu.ar>
2017-04-05 01:53:14 -0300

    Subo el proyecto del file system

---
+++ FileSystem/src/FileSystem.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : FileSystem.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
+#include <commons/config.h>
+#include <string.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
