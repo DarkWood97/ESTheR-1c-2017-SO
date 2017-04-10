0cff8e77212de28a8f428ad79d408ee39f07e862
Mathy977 <srabahia@est.frba.utn.edu.ar>
2017-04-10 01:35:56 -0300

    Tipos ip

--- Kernel/Debug/src/Kernel.d
+++ Kernel/Debug/src/Kernel.d
@@ -1,10 +1,10 @@
 src/Kernel.d: ../src/Kernel.c \
- /home/utnso/workspace/tp-2017-1c-caia/FuncionesGenericas/src/funcionesGenericas.h \
- /home/utnso/workspace/tp-2017-1c-caia/Socket/src/socket.h \
- /home/utnso/workspace/tp-2017-1c-caia/Socket/src/funcionesSockets.h
+ /home/utnso/workspace/tp-2017-1c-Caia/FuncionesGenericas/src/funcionesGenericas.h \
+ /home/utnso/workspace/tp-2017-1c-Caia/Socket/src/socket.h \
+ /home/utnso/workspace/tp-2017-1c-Caia/Socket/src/funcionesSockets.h
 
-/home/utnso/workspace/tp-2017-1c-caia/FuncionesGenericas/src/funcionesGenericas.h:
+/home/utnso/workspace/tp-2017-1c-Caia/FuncionesGenericas/src/funcionesGenericas.h:
 
-/home/utnso/workspace/tp-2017-1c-caia/Socket/src/socket.h:
+/home/utnso/workspace/tp-2017-1c-Caia/Socket/src/socket.h:
 
-/home/utnso/workspace/tp-2017-1c-caia/Socket/src/funcionesSockets.h:
+/home/utnso/workspace/tp-2017-1c-Caia/Socket/src/funcionesSockets.h:
