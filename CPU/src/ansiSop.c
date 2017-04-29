1325475abeea2406d03b893575a460cb6ec8a663
Mathy977 <srabahia@est.frba.utn.edu.ar>
2017-04-29 01:31:40 -0300

    Confeccionando parser

--- CPU/src/ansiSop.c
+++ CPU/src/ansiSop.c
@@ -8,5 +8,41 @@ t_puntero definirVariable(t_nombre_variable nombreVariable)
 }
 t_puntero obtenerPosicionVariable(t_nombre_variable identifcador_variable)
 {
+	return NULL;
+}
+t_puntero dereferenciar (t_puntero direccion_variable)
+{
+	return NULL;
+}
+void asignar (t_puntero direccion_variable, t_valor_variable valor)
+{
+
+}
+t_valor_variable obtenerValorCompartida (t_nombre_compartida variable)
+{
+	return NULL;
+}
+t_valor_variable asignarValorCompartida (t_nombre_compartida variable, t_valor_variable valor)
+{
+	return NULL;
+}
+void irAlLabel (t_nombre_etiqueta t_nombre_etiqueta)
+{
+
+}
+void llamarSinRetorno (t_nombre_etiqueta etiqueta)
+{
+
+}
+void llamarConRetorno (t_nombre_etiqueta etiqueta, t_puntero donde_retornar)
+{
+
+}
+void finalizar ()
+{
+
+}
+void retornar(t_valor_variable retorno)
+{
 
 }
