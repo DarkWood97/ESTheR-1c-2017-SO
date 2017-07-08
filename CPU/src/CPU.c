87a3aae050f8f4ee682dfa7c55bfa8c3202c7590
Gustavo Bitocchi <Gusbitocchi97@gmail.com>
2017-07-08 00:39:47 -0300

    Hiban Schedule.

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -158,9 +158,9 @@ char* pedirLineaAMemoria(){
 int main(int argc, char *argv[]) {
 	signal (SIGUSR1,chequeameLaSignal);
 	loggerCPU = log_create("./logCPU.txt", "CPU",0,0);
-	//verificarParametrosInicio(argc);
-	//inicializarCPU(argv[1]);
-	inicializarCPU("Debug/CPU.config");
+	verificarParametrosInicio(argc);
+	inicializarCPU(argv[1]);
+	//inicializarCPU("Debug/CPU.config");
 	log_info(loggerCPU, "CPU inicializada correctamente.");
 	//mostrarConfiguracionCPU(cpuDelSistema);
 	//Conecto
--- CPU/src/funcionesCpu.c
+++ CPU/src/funcionesCpu.c
@@ -145,23 +145,23 @@ void destruirPaquete(paquete * paquete)
 	free(paquete);
 }
 
-void* serializarPCB(PCB  pcb){
-	void* mensaje = malloc(sizeof(pcb));
-	memcpy(mensaje, &pcb.PID, sizeof(int));
-	memcpy(mensaje+sizeof(int), &pcb.ProgramCounter, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*2), &pcb.paginas_Codigo, sizeof(int));
-//	memcpy(mensaje+(sizeof(int)*3), &pcb.cod.comienzo, sizeof(int));
-//	memcpy(mensaje+(sizeof(int)*4), &pcb.cod.offset, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*5), pcb.etiquetas, sizeof(char)*16);
-	memcpy(mensaje+(sizeof(int)*5)+(sizeof(char)*16), &pcb.exitCode, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*6)+(sizeof(char)*16), &pcb.contextoActual, sizeof(t_list*)*16);
-	memcpy(mensaje+(sizeof(int)*6)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tamContextoActual, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*7)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tamEtiquetas, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*8)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.paginas, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*9)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.pid, sizeof(int));
-	memcpy(mensaje+(sizeof(int)*10)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.tamaniosPaginas, sizeof(int));
-	return mensaje;
-}
+//void* serializarPCB(PCB  pcb){
+//	void* mensaje = malloc(sizeof(pcb));
+//	memcpy(mensaje, &pcb.PID, sizeof(int));
+//	memcpy(mensaje+sizeof(int), &pcb.ProgramCounter, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*2), &pcb.paginas_Codigo, sizeof(int));
+////	memcpy(mensaje+(sizeof(int)*3), &pcb.cod.comienzo, sizeof(int));
+////	memcpy(mensaje+(sizeof(int)*4), &pcb.cod.offset, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*5), pcb.etiquetas, sizeof(char)*16);
+//	memcpy(mensaje+(sizeof(int)*5)+(sizeof(char)*16), &pcb.exitCode, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*6)+(sizeof(char)*16), &pcb.contextoActual, sizeof(t_list*)*16);
+//	memcpy(mensaje+(sizeof(int)*6)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tamContextoActual, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*7)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tamEtiquetas, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*8)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.paginas, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*9)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.pid, sizeof(int));
+//	memcpy(mensaje+(sizeof(int)*10)+(sizeof(char)*16)+(sizeof(t_list*)*16), &pcb.tablaKernel.tamaniosPaginas, sizeof(int));
+//	return mensaje;
+//}
 int _obtenerSizeActual_(int b)
 	{
 		int auxiliar=sizeof(int)*b+sizeof(char)*16+sizeof(t_list)*16;
